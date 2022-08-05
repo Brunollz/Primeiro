f79feee42d07a3a747454a350f49d896c47de359
Brunollz <limitlessbz@gmail.com>
2022-08-05 18:41:43 -0300

    aula1

--- aula1.py
+++ aula1.py
@@ -1,1 +1,9 @@
-print('hello world')
+
+km=input('quantos km foram percorridos?')
+horas=input('quantas horas levou a viagem?')
+
+
+print('o espaço percorrido foi de',km,'km.')
+print('o tempo que levou foi de',horas,'horas.')
+print('logo...')
+print(int(km)/int(horas),'km/h é a velocidade media do objeto!')
