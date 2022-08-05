07918eef6691f9766f5829e0cfa8a6243c39e507
Brunollz <limitlessbz@gmail.com>
2022-08-05 18:49:51 -0300

    novo teste de commit

--- aula1.py
+++ aula1.py
@@ -7,3 +7,4 @@ print('o espaço percorrido foi de',km,'km.')
 print('o tempo que levou foi de',horas,'horas.')
 print('logo...')
 print(int(km)/int(horas),'km/h é a velocidade media do objeto!')
+#nota
