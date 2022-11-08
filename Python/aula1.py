4446c345632abdb479166b09af42920d38819270
Brunollz <limitlessbz@gmail.com>
2022-11-08 20:53:21 -0300

    Aulas de HTML

---
+++ Python/aula1.py
@@ -0,0 +1,10 @@
+
+km=float(input('quantos km foram percorridos?'))
+horas=float(input('quantas horas levou a viagem?'))
+KmH=str(km/horas) 
+
+print('o espaço percorrido foi de',km,'km.')
+print('o tempo que levou foi de',horas,'horas.')
+print('logo...')
+print('{} km/h é a velocidade media do objeto!'.format(KmH))
+#nota
---
+++ Python/teste.py
@@ -0,0 +1,7 @@
+
+num = int(input('Digite um número: '))
+if num % 2 == 0:
+        print(f'O Número {num} é PAR')
+else:
+        print(f'O número {num} é ÍMPAR')
+print('Fim do Programa')
